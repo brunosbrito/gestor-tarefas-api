205f8a9494a784407fbf1489c6f89d81377cd4d9
Bruno <digitalgml@gmlindustrial.com.br>
2025-01-22 13:54:20 -0300

    update cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: '*',
+    origin: [
+      'https://gestor-tarefas.gmxindustrial.com.br',
+      'http://localhost:4200',
+    ],
     methods: 'GET,POST,PUT,DELETE,PATCH',
     allowedHeaders: 'Content-Type, Authorization',
   });
