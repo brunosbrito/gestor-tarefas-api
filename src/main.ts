655f9875068c913b9efdbd822112945b493d59ad
Bruno <digitalgml@gmlindustrial.com.br>
2026-01-28 22:34:19 -0300

    update

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { json, urlencoded } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Aumentar limite de upload para 50MB
+  app.use(json({ limit: '50mb' }));
+  app.use(urlencoded({ extended: true, limit: '50mb' }));
 
   app.enableCors({
     origin: true,
